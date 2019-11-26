982901ca07d0c6d325c3d8de6937d3763d5ccc90
Nathanael <50619296+NathanaelZondo@users.noreply.github.com>
2019-11-26 13:18:38 +0200

    fdsgd

--- src/app/pages/currentmatch/currentmatch.page.ts
+++ src/app/pages/currentmatch/currentmatch.page.ts
@@ -13,52 +13,33 @@ export class CurrentmatchPage implements OnInit {
 currentmatch =[];
 timer;
 docid;
+mins =0;
 sub :Subscription;
+btntxt1 ="First Half";
+btntxt2 ="Second Half";
+btn1 =false;
+btn2 =true;
   constructor(public plt:Platform,public serve:AllserveService,public router:Router) {
 
-    this.plt.ready().then(() =>{
-      this.sub = timer(0,10).subscribe(result =>{
-       
+    firebase.firestore().collection('MatchFixtures').doc(this.serve.currentmatch.id).get().then(val=>{
 
-
-
-
-
-
-console.log('docid = ',this.serve.currentmatch.id)
-
-        firebase.firestore().collection('MatchFixtures').doc(this.serve.currentmatch.id).get().then(val=>{
-
-          // val.forEach(res=>{
-          //   console.log({id:res.id,...res.data()})
-          //   this.timer=res.data();
-          //   this.docid =res.id;
-          //   console.log("time = ",this.timer)
-          // })
-          // console.log(val.data())
-this.timer =val.data().timer;
-          console.log(val.data().timer)
-
-          firebase.firestore().collection('MatchFixtures').doc(this.serve.currentmatch.id).update({timer:this.timer+1});
-        })
     
-    
-    
-        
-
-
-
-
-
-
-
-
-
+      if(val.data().mins>0&&val.data().mins<=46)
+      {
+       this.btntxt1 ="Resume First Half";
       }
-      )})
+      else  if(val.data().mins>0&&val.data().mins<=90)
+      {
+        this.btntxt2 ="Resume Second Half";
+      }
 
 
 
+              
+              
+         })
+      
+      
 
    }
 
@@ -71,6 +52,106 @@ ionViewWillEnter()
   this.currentmatch.push(this.serve.currentmatch);
   console.log("currentmatch = ",this.currentmatch);
 }
+
+
+
+
+firsthalf()
+{
+  this.btn1 =true;
+  this.btn2 =true;
+  
+    this.sub = timer(0,1000).subscribe(result =>{
+     
+
+
+console.log('docid = ',this.serve.currentmatch.id)
+
+      firebase.firestore().collection('MatchFixtures').doc(this.serve.currentmatch.id).get().then(val=>{
+
+    
+this.timer =val.data().timer;
+        console.log(val.data().timer)
+        
+        if(this.timer==59)
+        {
+this.timer =0;
+this.mins=this.mins+1;
+
+        }
+
+
+        firebase.firestore().collection('MatchFixtures').doc(this.serve.currentmatch.id).update({timer:this.timer+1,mins:this.mins});
+      })
+  
+  
+  
+    })
+
+
+
+
+
+
+
+
+
+   
+
+
+
+}
+
+stop()
+{
+  this.btn1 =true;
+  this.btn2 =false;
+  this.sub.unsubscribe();
+  console.log(this.sub.unsubscribe())
+}
+
+secondhalf()
+{
+  this.btn1 =true;
+  this.btn2 =true;
+  this.sub = timer(0,1000).subscribe(result =>{
+     
+
+
+    console.log('docid = ',this.serve.currentmatch.id)
+    
+          firebase.firestore().collection('MatchFixtures').doc(this.serve.currentmatch.id).get().then(val=>{
+    
+    this.timer =val.data().timer;
+            console.log(val.data().timer)
+            
+            if(this.timer==59)
+            {
+    this.timer =0;
+    this.mins=this.mins+1;
+    
+            }
+    
+    
+            firebase.firestore().collection('MatchFixtures').doc(this.serve.currentmatch.id).update({timer:this.timer+1,mins:this.mins});
+          })
+      
+      
+      
+        })
+    
+    
+    
+    
+    
+
+
+
+}
+
+
+
+
 
 
 }
