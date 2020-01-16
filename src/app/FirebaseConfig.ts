40e10a15392b32686f50f231058b1c95366cd371
seko17 <34337236+seko17@users.noreply.github.com>
2020-01-16 10:17:36 +0200

    seko

--- src/app/FirebaseConfig.ts
+++ src/app/FirebaseConfig.ts
@@ -6,6 +6,6 @@ export const config = {
   storageBucket: "testtournamentdb.appspot.com",
   messagingSenderId: "81311888576",
   appId: "1:81311888576:web:896c7c5eadf5fab1166925",
-  measurementId: "G-8KZEPZZC8R"
+  measurementId: "G-8KZEPZZC8R",
   vapidKey: "BGDfUFFnEacPz8kO1Y_aMEOeoBjS6hG652svWfiJwItjgSQZhNTFvN9Xam240tTMF3tJ45KQ54slIWs-z9XJfGA"
 }
