ffe21516b92af14c03ba9d9c53ccc0d0588a6073
seko17 <34337236+seko17@users.noreply.github.com>
2020-02-06 17:20:54 +0200

    seko

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '../app/guards/auth.guard';
+import { AuthGuard } from './services/user/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full',canActivate: [AuthGuard] },
